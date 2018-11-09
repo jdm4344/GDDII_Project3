4e8fbf2f52a511d14ce8a58d3def618737311292
Jordan Machalek <jdm4344@rit.edu>
2018-11-09 09:57:35 -0500

    Began work on menu functionality

---
+++ GDDII_Project3/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    // Variables
+    public List<string> minigames; // List of names of scenes
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Randomly selects and loads a minigame from the 'minigames' list
+    /// To be attached to 'ChooseGameButton' object in menu canvas
+    /// </summary>
+    public void PickGame()
+    {
+
+    }
+}
