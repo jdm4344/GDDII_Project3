23e30358d5ddf42c4b2bc378624e74f6606418f3
Jordan Machalek <jdm4344@rit.edu>
2018-11-12 09:52:47 -0500

    Worked on menu

--- GDDII_Project3/Assets/Scripts/MenuManager.cs
+++ GDDII_Project3/Assets/Scripts/MenuManager.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * 
+ */
 public class MenuManager : MonoBehaviour {
 
     // Variables
+    // Minigame specific variables
     public List<string> minigames; // List of names of scenes
+
+    // Board-space specific variables
+    public int totalSpaces;
 
 	// Use this for initialization
 	void Start () {
@@ -21,8 +27,19 @@ public class MenuManager : MonoBehaviour {
     /// Randomly selects and loads a minigame from the 'minigames' list
     /// To be attached to 'ChooseGameButton' object in menu canvas
     /// </summary>
-    public void PickGame()
+    public void PickMinigame()
+    {
+        int gameNum = Random.Range(0, minigames.Count);
+
+
+    }
+
+    /// <summary>
+    /// Randomly selects a position for a new checkpoint
+    /// </summary>
+    public void PickNewCheckpoint()
     {
 
     }
+
 }
