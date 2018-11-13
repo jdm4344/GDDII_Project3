a4489f1e88979d97e5dc081bcff3e8a370a8b533
Jordan Machalek <jdm4344@rit.edu>
2018-11-13 18:26:41 -0500

    Minigame selection works - still requires code for loading apropriate scene

--- GDDII_Project3/Assets/Scripts/MenuManager.cs
+++ GDDII_Project3/Assets/Scripts/MenuManager.cs
@@ -8,19 +8,27 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour {
 
     // Variables
+    public GameObject menuPanel;
     // Minigame specific variables
+    public GameObject minigamePanel;    
     public List<string> minigames; // List of names of scenes
+    private Stack<string> playedMinigames; // Keeps track of minigames that have been played, clears when all have been played once
+    public GameObject iconContainer;
+    [SerializeField]
+    private List<GameObject> icons;
 
     // Board-space specific variables
+    public GameObject mapPanel;    
     public GameObject spaceContainer; // Gameobject whose children are spaces on the board
     [SerializeField]
     private List<GameObject> spaces;
-    public GameObject menuPanel;
-    public GameObject mapPanel;
+    
+    
 
 	// Use this for initialization
 	void Start () 
     {
+        playedMinigames = new Stack<string>();
         spaces = new List<GameObject>();
 
         // Add all of the spaces to the list of spaces
@@ -28,11 +36,19 @@ public class MenuManager : MonoBehaviour {
         {
             spaces.Add(child.gameObject);
         }
+
+        // Add all of the icons to the list of icons
+		foreach(Transform child in iconContainer.transform)
+        {
+            icons.Add(child.gameObject);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        // Clear the stack of played minigames if they have all been played
+	    if(playedMinigames.Count == minigames.Count) {playedMinigames.Clear(); }	
 	}
 
     /// <summary>
@@ -41,9 +57,20 @@ public class MenuManager : MonoBehaviour {
     /// </summary>
     public void PickMinigame()
     {
-        int gameNum = Random.Range(0, minigames.Count);
+        // Swap UI
+        ToggleMinigames();
 
+        int gameNum;
 
+        while(true)
+        {
+            gameNum = Random.Range(0, minigames.Count);
+            if(!playedMinigames.Contains(minigames[gameNum])) { break;}
+        }
+
+        Debug.Log(gameNum);
+        playedMinigames.Push(minigames[gameNum]);
+        icons[gameNum].GetComponent<RawImage>().color = Color.red;
     }
 
     /// <summary>
@@ -63,7 +90,6 @@ public class MenuManager : MonoBehaviour {
             // Check if the space is not a shop, if so, break
             // If it's a shop, choose a new space
             if(spaces[spaceNum].tag != "Shop Space") { break;}
-        
         }
 
         spaces[spaceNum].GetComponent<RawImage>().color = Color.red;
@@ -84,6 +110,22 @@ public class MenuManager : MonoBehaviour {
         foreach(GameObject space in spaces)
         {
             if(space.tag != "Shop Space") {space.GetComponent<RawImage>().color = spaceColor;}
+        }
+    }
+
+    /// <summary>
+    /// Helper Method
+    /// Shows/hides main menu and minigame panels
+    /// </summary>
+    public void ToggleMinigames()
+    {
+        menuPanel.SetActive(!menuPanel.activeSelf);
+        minigamePanel.SetActive(!minigamePanel.activeSelf);
+
+        // Reset colors of game icons
+        foreach(GameObject icon in icons)
+        {
+            icon.GetComponent<RawImage>().color = Color.white;
         }
     }
 }
