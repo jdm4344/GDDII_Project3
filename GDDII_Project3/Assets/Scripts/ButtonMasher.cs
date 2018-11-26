60c4ff1211d2d8ae28c8f8117c22b83fc7b4cdc0
Matt <mxr2091@g.rit.edu>
2018-11-26 16:26:11 -0500

    Added Gui for percentages and winner, linked

--- GDDII_Project3/Assets/Scripts/ButtonMasher.cs
+++ GDDII_Project3/Assets/Scripts/ButtonMasher.cs
@@ -13,22 +13,44 @@ public class ButtonMasher : MonoBehaviour {
     // keep track of how many times the players have hit the buttons
     public int[] buttonPresses;
 
+    // percentage complete
+    public int[] percentages;
+
     // how many to win?
     public const int pressesToWin = 100;
 
     // is the game over
     public bool gameOver;
 
+    // the winner
+    public int winner;
+
     // names of players and their fame earned from this minigame
     public Dictionary<string, int> results = new Dictionary<string, int>();
+
+    // access to the player manager script on the scene manager object
+    public PlayerManager playerManager;
+
+    void Awake()
+    {
+        playerManager = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+    }
 
     // Use this for initialization
     void Start ()
     {
-        //numOfPlayers = 
+        // how many players are playing?
+        numOfPlayers = playerManager.playerNames.Length;
+        // setup
         buttonPresses = new int[numOfPlayers];
         playerNames = new string[numOfPlayers];
+        percentages = new int[numOfPlayers];
         gameOver = false;
+        // get player names
+        for (int i = 0; i < numOfPlayers; i++)
+        {
+            playerNames[i] = playerManager.playerNames[i];
+        }
     }
 	
 	// Update is called once per frame
@@ -40,19 +62,43 @@ public class ButtonMasher : MonoBehaviour {
             ButtonPressed();
 
             // check for a win
-            CheckWinCondition();
-        }        
+            winner = CheckWinCondition() - 1;
+
+            DisplayPercentage();
+        }
+        DisplayWinner();
 	}
+
+    // show the winner
+    void DisplayWinner()
+    {
+        GUI.TextArea(new Rect(0f, 3f, 6f, 2f), "Player " + playerNames[winner] + " has won!");
+    }
+
+    // draw the percentages on screen
+    void DisplayPercentage()
+    {
+        for (int i = 0; i < numOfPlayers; i++)
+        {
+            // make a new text box, shifts over for each player
+            GUI.TextArea(new Rect(-6f + 4f * i, 2f, 3f, 1f), percentages[i] + "%");
+        }
+    }
 
     // adds to the button count of the player who pressed it
     void ButtonPressed()
     {
         // loop through each player in the game
-        for (int i = 0; i < numOfPlayers; i++)
+        for (int i = 1; i < numOfPlayers + 1; i++)
         {
             // check if the A button has been pressed this frame (NOT a hold)
             if (Input.GetButtonDown(i + "A"))
                 buttonPresses[i]++; // adds to the count
+        }
+
+        for (int i = 0; i < numOfPlayers; i++)
+        {
+            percentages[i] = buttonPresses[i] / pressesToWin * 100;
         }
     }
 
@@ -83,7 +129,7 @@ public class ButtonMasher : MonoBehaviour {
         for (int i = 0; i < numOfPlayers; i++)
         {
             // add to the dictionary
-            results.Add(playerNames[i - 1], CalcFameEarned(buttonPresses[i - 1]));
+            results.Add(playerNames[i], CalcFameEarned(buttonPresses[i]));
         }
     }
 
